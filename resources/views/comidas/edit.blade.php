d220f7465a447853386fd1aa7e48d34e4c95fd4a
Maestro <Maestro@iMac-de-Maestro-9.local>
2018-04-09 18:38:51 -0700

    hola

--- resources/views/comidas/edit.blade.php
+++ resources/views/comidas/edit.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Comidas</title>
-</head>
-<body>
+@extends('layouts.default')
+@section('content')
     <h1>Editar comida</h1>
     <a href="{{route('comidas.index')}}">Volver a comidas</a>
     @if($exito)
@@ -14,19 +7,31 @@
     @endif
     <form method="POST" action="{{ route('comidas.update',array('comidas'=>$comida->id)) }}">
         {{ csrf_field() }}
-        <input type="hidden" name="_method" value="PUT">
-        <label>Nombre:</label>
-        <input type="text" name="txtNombre" value="{{$comida->nombre}}">
-        <label>Precio:</label>
-        <input type="text" name="txtPrecio" value="{{$comida->precio}}">
-        <label>Descripcion:</label>
-        <input type="text" name="txtTexto" value="{{$comida->texto}}">
-        <button type="submit">Actualizar comida</button>
+        <div class="form-group">
+        <input class="form-control"  type="hidden" name="_method" value="PUT">
+        <label class="form-label">Nombre:</label>
+        </div>
+        <div class="form-group">
+        <input class="form-control"  type="text" name="txtNombre" value="{{$comida->nombre}}">
+        <label class="form-label">Precio:</label>
+        </div>
+        <div class="form-group">
+        <input class="form-control"  type="text" name="txtPrecio" value="{{$comida->precio}}">
+        <label class="form-label">Descripcion:</label>
+        </div>
+        <div class="form-group">
+        <input class="form-control" type="text" name="txtTexto" value="{{$comida->texto}}">
+        <button class="btn btn-primary" type="submit">Actualizar comida</button>
     </form>
-    <form method="POST" action="{{route('comidas.destroy',array('comidas'=>$comida->id))}}">
+    <div class="form-group">
+    <form  method="POST" action="{{route('comidas.destroy',array('comidas'=>$comida->id))}}">
         {{ csrf_field() }}
-        <input type="hidden" name="_method" value="DELETE">
-        <button type="submit">Borrar comida</button>
+       
+      
+        <input class="form-control" type="hidden" name="_method" value="DELETE">
+    
+        <button class="btn btn-danger" type="submit">Borrar comida</button>
+</div>
     </form>
-</body>
-</html>
+
+@endsection
--- resources/views/comidas/index.blade.php
+++ resources/views/comidas/index.blade.php
@@ -3,9 +3,9 @@
 <h1>Comidas</h1>
 @if($exito != null)
     @if($exito == 1)
-        <p>Se ha agregado una comida</p>
+        <p class="text-success">Se ha agregado una comida</p>
     @else
-        <p>No se ha podido agregar comida</p>
+        <p class="text-danger">No se ha podido agregar comida</p>
     @endif
 @endif 
 
@@ -18,9 +18,9 @@
 @endif 
 
 <a href="{{route('comidas.create')}}">
-    <button>Agregar comida</button>
+    <button class="btn btn-primary">Agregar comida</button>
 </a>
-<table>
+<table class="table table-striped">
     <thead>
         <tr>
             <th>Nombre</th>
@@ -37,7 +37,7 @@
                 <td>{{$comida->texto}}</td>
                 <td>
                     <a href="{{route('comidas.edit',$comida->id)}}">
-                        <button>Editar</button>
+                        <button class="btn btn-primary">Editar</button>
                     </a>
                 </td>
             </tr>
